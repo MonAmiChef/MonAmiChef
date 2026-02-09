60f143e3d3e4eca53fbdeeda4444865470d56905
Lenny <61317098+GarnierLenny@users.noreply.github.com>
2026-02-09 18:02:46 +0100

    Meal plan controller (#27)
    * ai now replies is message isRecipe with isRecipe
    
    * show add to meal plan button
    
    * now parses recipe and ingredients and puts in mealplan
    
    * retrieves meal image from unsplash
    
    * meal plan backend ready
    
    * adjust json
    
    * meal plan controller

--- backend/src/meal-plan/meal-plan.controller.ts
+++ backend/src/meal-plan/meal-plan.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { MealPlanService } from './meal-plan.service';
 import {
   SupabaseGuard,
   type AuthenticatedRequest,
 } from 'src/supabase-guard/supabase.guard';
+import {
+  AddMealToGroceriesRequestDto,
+  RemoveFromMealPlanRequestDto,
+} from './meal-plan.dto';
 
 @Controller('meal-plan')
 @UseGuards(SupabaseGuard)
@@ -13,5 +17,25 @@ export class MealPlanController {
   @Get()
   async getUserMealPlan(@Req() req: AuthenticatedRequest) {
     return this.mealPlanService.getUserMealPlan(req.user.id);
+  }
+
+  @Post('remove')
+  async removeMealFromMealPlan(
+    @Req() req: AuthenticatedRequest,
+    @Body() body: RemoveFromMealPlanRequestDto,
+  ) {
+    return this.mealPlanService.removeFromMealPlan(req.user.id, body.recipeId);
+  }
+
+  @Post('add-groceries')
+  async addRemoveMealToGroceries(
+    @Req() req: AuthenticatedRequest,
+    @Body() body: AddMealToGroceriesRequestDto,
+  ) {
+    return this.mealPlanService.addRemoveMealToGroceries(
+      req.user.id,
+      body.recipeId,
+      body.newState,
+    );
   }
 }
--- backend/src/meal-plan/meal-plan.service.ts
+++ backend/src/meal-plan/meal-plan.service.ts
@@ -8,4 +8,20 @@ export class MealPlanService {
   async getUserMealPlan(userId: string) {
     return this.mealPlanRepository.getUserMealPlan(userId);
   }
+
+  async removeFromMealPlan(userId: string, recipeId: string) {
+    return this.mealPlanRepository.removeFromMealPlan(userId, recipeId);
+  }
+
+  async addRemoveMealToGroceries(
+    userId: string,
+    recipeId: string,
+    newState: boolean,
+  ) {
+    return this.mealPlanRepository.addRemoveMealToGroceries(
+      userId,
+      recipeId,
+      newState,
+    );
+  }
 }
